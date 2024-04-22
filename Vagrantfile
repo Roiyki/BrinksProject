c46fe675aaec0ef1fede27ceb4ec878f8f318894
Roiyki <141772658+Roiyki@users.noreply.github.com>
2024-04-22 15:32:47 +0300

    some changes in the docker compose and nginx setup script

--- Vagrantfile
+++ Vagrantfile
@@ -16,7 +16,7 @@ Vagrant.configure("2") do |config|
     server.vm.network "forwarded_port", guest: 8080, host: 8090
     server.vm.network "forwarded_port", guest: 80, host: 8888
     server.vm.network "forwarded_port", guest: 3000, host: 3000
-    server.vm.network "forwarded_port", guest: 10050, host: 10050
+    server.vm.network "forwarded_port", guest: 10051, host: 10051
     server.vm.network "private_network", ip: "192.168.50.10"
     # Creating linked directories between docker volumes and local directories
     server.vm.synced_folder ".", "/home/vagrant/local"
@@ -31,5 +31,6 @@ Vagrant.configure("2") do |config|
   config.vm.provision "shell", path: "provisioning/docker.sh"
   config.vm.provision "shell", path: "provisioning/repo_setup.sh"
   config.vm.provision "shell", path: "provisioning/nginx_setup.sh"
+  config.vm.provision "shell", path: "provisioning/agent_setup.sh"
   end
 end
--- provisioning/nginx_setup.sh
+++ provisioning/nginx_setup.sh
@@ -4,14 +4,17 @@ echo "Provisioning script #6 is running"
 # Get the container ID
 CONTAINER_ID=$(docker ps -qf "name=dockerconf_zabbix-web_1")
 
-# Create necessary directories
+# Step 1: Copy the Nginx configuration file from the container
+docker cp $CONTAINER_ID:/etc/zabbix/nginx.conf nginx.conf
 
-# Configure Nginx to serve the Zabbix web interface
-docker exec $CONTAINER_ID sed -i 's/listen 8080;/listen 80;/g' /etc/zabbix/nginx.conf
+# Step 2: Modify the Nginx configuration file
+sed -i 's/listen 8080;/listen 80;/g' nginx.conf
 
-# Create necessary directories
-docker exec $CONTAINER_ID nginx -s reload 
-sed -i "s/zabbix-server:.*/zabbix-server:$ZABBIX_SERVER_IP/" docker-compose.yml
+# Step 3: Copy the modified Nginx configuration file back to the container
+docker cp nginx.conf $CONTAINER_ID:/etc/zabbix/nginx.conf
+
+# Step 4: Reload Nginx inside the container
+docker exec -u 0 $CONTAINER_ID nginx -s reload
 
 echo "Nginx configured to serve Zabbix web interface on port 80"
 echo "All done"
